9e180ce4ba65604d07015e8fc60de48e89773bf1
Shravani Tambe <shravani.tambe@Gmail.com>
2024-08-30 23:48:10 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ def main():
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
         exit(1)
     
-    command = sys.argv[1] #no of arguments
-    filename = sys.argv[2] #name of file
+    command = sys.argv[1] 
+    filename = sys.argv[2] 
     
     if command != "tokenize":
         print(f"Unknown command: {command}", file=sys.stderr)
@@ -18,10 +18,10 @@ def main():
         file_contents = file.read()
     
     if file_contents:
-        for i in file_contents:
-            if i == "(":
+        for c in file_contents:
+            if c == "(":
                 print("LEFT_PAREN ( null")
-            elif i == ")":
+            elif c == ")":
                 print("RIGHT_PAREN ) null")
     
     print("EOF  null")
