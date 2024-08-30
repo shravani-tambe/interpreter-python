b70c0f40dae4275bc68bd3bf301db64188795762
Shravani Tambe <shravani.tambe@Gmail.com>
2024-08-30 23:19:55 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,28 +1,32 @@
 import sys
+
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
+    
     if len(sys.argv) < 3:
         print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
         exit(1)
+    
     command = sys.argv[1]
     filename = sys.argv[2]
+    
     if command != "tokenize":
         print(f"Unknown command: {command}", file=sys.stderr)
         exit(1)
+    
     with open(filename) as file:
         file_contents = file.read()
+    
     if file_contents:
-        raise NotImplementedError("Scanner not implemented")
+        for c in file_contents:
+            if c == "(":
+                print("LEFT_PAREN ( null")
+            elif c == ")":
+                print("RIGHT_PAREN ) null")
     else:
-        print(
-            "EOF  null"
-        )  # Placeholder, remove this line when implementing the scanner
-    for c in file_contents:
-        if c == "(":
-            print("LEFT_PAREN ( null")
-        if c == ")":
-            print("RIGHT_PAREN ) null")
-    print("EOF  null")
+        print("EOF  null")  # Handle empty file case
+    
+    print("EOF  null")  # Ensure EOF is printed at the end
+
 if __name__ == "__main__":
     main()
