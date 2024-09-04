d61caece60ff05096a9164d099306209aa2182b7
Shravani Tambe <shravani.tambe@Gmail.com>
2024-09-04 21:58:05 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -27,43 +27,33 @@ def main():
         
         while ptr < len(file_contents):
             i = file_contents[ptr]
-            char_name = " "
+            char_name = None  
+            
             if i == "(":
-                print("LEFT_PAREN ( null")
                 char_name = "LEFT_PAREN"
             elif i == ")":
-                print("RIGHT_PAREN ) null")
                 char_name = "RIGHT_PAREN"
             elif i == "{":
-                print("LEFT_BRACE { null")
                 char_name = "LEFT_BRACE"
             elif i == "}":
-                print("RIGHT_BRACE } null")
                 char_name = "RIGHT_BRACE"
             elif i  == "*": 
-                print("STAR * null")
                 char_name = "STAR"
             elif i == ".": 
-                print("DOT . null")
                 char_name = "DOT"
             elif i == ",": 
-                print("COMMA , null")
                 char_name = "COMMA"
             elif i == "+": 
-                print("PLUS + null")
                 char_name = "PLUS"
             elif i == "-": 
-                print("MINUS - null")
                 char_name = "MINUS"
             elif i == ";": 
-                print("SEMICOLON ; null")
                 char_name = "SEMICOLON"
             elif i == "=": 
-                print("EQUAL = null")
                 if ptr < len(file_contents) - 1 and file_contents[ptr + 1] == "=":
                     char_name = "EQUAL_EQUAL"
-                    i = "=="
-                    ptr += 1 
+                    i = "=="  
+                    ptr += 1  
                 else:
                     char_name = "EQUAL"
             elif i == "\n":
@@ -76,12 +66,14 @@ def main():
                 ptr += 1
                 continue
             
+            if char_name:  
+                toks.append(f"{char_name} {i} null")
             ptr += 1 
-            toks.append(f"{char_name} {i} null")
         
-        toks.append("EOF  null")
-        print("\n".join(errs), file=sys.stderr)
-        print("\n".join(toks))
+        toks.append("EOF  null")  
+        print("\n".join(toks))  
+        if errs:
+            print("\n".join(errs), file=sys.stderr)
     
     if error:
         exit(65)
